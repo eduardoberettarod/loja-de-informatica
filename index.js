940a678f779345679037425b50aefe6ce50d7a41
Eduardo Beretta <eduardo.berettarod@gmail.com>
2026-02-06 22:16:23 -0300

    adicionando filtro
    adicionando botao de filtro, e fazendo com que dependendo do tipo de filtro que escolher, ele muda o nome.

--- index.js
+++ index.js
@@ -42,17 +42,18 @@ let conexao = mysql.createConnection({
     database: "gutoxa27_bd_loja"
 })
 
-conexao.connect(function (erro){
-    if(erro) {
+conexao.connect(function (erro) {
+    if (erro) {
         console.log("Deu ruim na conexão \n");
         throw erro;
-    }else{
+    } else {
         console.log("Conexão deu bom \n")
     }
 })
 
+
 // Read All - [GET] /produtos
-app.get("/produtos", function (req, res){
+app.get("/produtos", function (req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*')
     // res.send(lista_produtos)
 
@@ -62,15 +63,33 @@ app.get("/produtos", function (req, res){
     })
 })
 
-app.get("/produtos/:categoria", function (req,res){
+app.get("/produtos/:categoria", function (req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*')
+
     const categoria = req.params.categoria
-    conexao.query(`SELECT * FROM produtos where categoria='${categoria}'`, function (erro, dados, campos){
+
+    conexao.query(`SELECT * FROM produtos where categoria='${categoria}'`, function (erro, dados, campos) {
         res.send(dados)
     })
 })
 
-app.get("/unidades", function (req, res){
+app.get("/produtos/:categoria/:ordem", function (req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+
+    const ordem = req.params.ordem
+    const categoria = req.params.categoria
+
+    conexao.query(`SELECT * FROM produtos WHERE categoria='${categoria}' ORDER BY ${ordem} ASC`, function (erro, dados, campos) {
+        if (erro) {
+            console.log(erro)
+        }
+        res.send(dados)
+    })
+
+})
+
+
+app.get("/unidades", function (req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*')
 
     conexao.query("SELECT * FROM unidades", function (erro, lista_unidades, campos) {
--- site-loja/app.js
+++ site-loja/app.js
@@ -3,7 +3,7 @@ function fnMontarCardProduto(produto) {
         <div class="col-12 col-sm-12 col-md-6 col-lg-4 mb-3">
                 <div class="card">
                     <img src="${produto.foto}"
-                        class="card-img-top" style="width:100%; height:300px; object-fit:cover; " alt="${produto.nome}">
+                        class="card-img-top" style="width:100%; height:300px; object-fit:cover; " alt="${produto.titulo}">
                     <div class="card-body">
                         <h5 class="card-title">${produto.titulo}</h5>
                         <p class="card-text">${produto.descricao}</p>
@@ -32,16 +32,54 @@ function fnMontarCardProduto(produto) {
 function fnCarregarDados() {
 
     const parametros = new URLSearchParams(window.location.search)
-    const existe_categoria = parametros.has('categoria')
+
+    const nomeFiltrado = document.getElementById("nomeFiltrado")
+    if (parametros.has('ordem')) {
+
+        const ordem = parametros.get('ordem')
+
+        if (ordem === "preco") {
+            nomeFiltrado.innerHTML = "Ordenar por preço"
+        }
+        else if (ordem === "titulo") {
+            nomeFiltrado.innerHTML = "Ordenar por título"
+        }
+
+        } else {
+            nomeFiltrado.innerHTML = "Filtro"
+        }
+
+        const existe_categoria = parametros.has('categoria')
 
     let rota_categoria = ''
+
+    const filtro = document.querySelector(".filtro")
+    const categoria = parametros.get('categoria')
+    const btnFiltro = document.querySelector('.btn-filtro')
+
+
+    filtro.innerHTML =
+        `<li><a href="produtos.html?categoria=${categoria}&ordem=preco" class="dropdown-item">Ordenar por preço</a></li>
+        <li><a href="produtos.html?categoria=${categoria}&ordem=titulo" class="dropdown-item">Ordenar por titulo</a></li>`
+
     if (existe_categoria) {
         rota_categoria = parametros.get('categoria') + "/"
+    }else {
+        btnFiltro.style.display = "none"
     }
-        fetch('http://localhost:3000/produtos/' + rota_categoria, { method: 'GET'})
 
-        
-    fetch('http://localhost:3000/produtos/', { method: 'GET' })
+
+    const existe_ordem = parametros.has('ordem')
+
+    let rota_ordem = ''
+    if (existe_ordem) {
+        rota_ordem = parametros.get('ordem') + "/"
+    }
+
+    document.querySelector(".lista-produtos").innerHTML = ""
+
+
+    fetch('http://localhost:3000/produtos/' + rota_categoria + rota_ordem, { method: 'GET' })
         .then(response => response.json())
         .then((produtos) => {
             produtos.forEach(produto => {
